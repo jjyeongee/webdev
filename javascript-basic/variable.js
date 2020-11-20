fee14a1b4d54f8dbe21478feb1a0acb1f0aebfbb
jiyoung <jiyoung9159k@gmail.com>
2020-11-20 23:12:19 +0900

    add dynamic typing

--- javascript-basic/variable.js
+++ javascript-basic/variable.js
@@ -58,6 +58,49 @@ const char = 'c';
 const brendan = 'brendan';
 const greeting = 'hello' + brendan;
 console.log('value: ${greeting}, type: ${typeof greeting}');
-const helloBob = 'hi ${brendan}!'; // template literals (string)
-console.log('value: ${helloBob}, type: ${typeof helloBob}');
+const helloBob = `hi ${brendan}!`; // template literals (string) `` 백틱기호 
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+//boolean
+//false: 0, null, undefined, NaN, ''
+//true: any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`);
+console.log(`value: ${test}, type: ${typeof test}`);
+
+// null
+let nothing = null; //너는 텅텅빈 값이야 아무것도 아니야
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+//undefined
+let x; // 선언은 되었지만 아무것도 값이 지정되지 않음 값이 있는지 없는지 정해지지 않음
+console.log(`value: ${x}, type: ${typeof x}`);
  
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol === symbol2);
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`);
+// 동일한 스트링을 작성했어도 다른 심볼로 만들어짐
+// 심볼은 주어진 스트링에 상관없이 고유한 식별자를 만들 때 사용
+
+// object, real-life object, data structure
+const jiyoung = {name: 'jiyoung', age: 20}; //한번 할당된 object는 다시는 다른 object로 변경 불가
+jiyoung.age = 21; // 이렇게 변경 가능
+
+// 5. Dynamic typing : dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0)); // h출력
+console.log(`value: ${text}, type: ${typeof test}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof test}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof test}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof test}`);
+console.log(text.charAt(0)); // boom!
+
